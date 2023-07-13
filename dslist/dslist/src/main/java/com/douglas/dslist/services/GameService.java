ed2bad57823cdefba7ea9488969edb709ce230ff
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-07-12 22:43:42 -0300

    controller findById

--- dslist/dslist/src/main/java/com/douglas/dslist/services/GameService.java
+++ dslist/dslist/src/main/java/com/douglas/dslist/services/GameService.java
@@ -4,24 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.douglas.dslist.dto.GameDTO;
 import com.douglas.dslist.dto.GameMinDTO;
 import com.douglas.dslist.entities.Game;
 import com.douglas.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-	
-	
+		
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x-> new GameMinDTO(x)).toList();
 		return dto;
 	}
-	
-	
 
 }
